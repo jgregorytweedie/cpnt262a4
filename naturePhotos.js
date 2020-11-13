4640d3def20d0d2e4409866db04557ee1d3bd9bf
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-12 22:30:54 -0700

    added in a bajillion things

--- naturePhotos.js
+++ naturePhotos.js
@@ -4,33 +4,33 @@ const naturePhotos = [
         title: "photo1",
         description: "This one is very neat",
         credit: "https://unsplash.com/photos/MF16lGb95WY",
-        image_path: ""
+        image_path: "/images/photo1"
     },
     {
         title: "photo2",
         description: "Neater",
         credit: "https://unsplash.com/photos/DuiPYwz3CBA",
-        image_path: "",
+        image_path: "/images/photo2",
     },
     {
         title: "photo3",
         description: "Neatest",
         credit: "https://unsplash.com/photos/o_-gToPk62c",
-        image_path: "",
+        image_path: "/images/photo3",
     },
     {
         title: "photo4",
         description: "Neatester",
         credit: "https://unsplash.com/photos/zwzeorQPepo",
-        image_path: "",
+        image_path: "/images/photo4",
     },
     {
         title: "photo5",
         description: "Neatesterest",
         credit: "https://unsplash.com/photos/EKLr56NaJDw",
-        image_path: "",
+        image_path: "/images/photo5",
     },
 ]
 
 // I wanna export this as a module to use in my server.
-module.exports = stadiums;
+module.exports = naturePhotos;
