20a37de86f4029746f350785840fe65b30043f0b
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-12 22:01:00 -0700

    added in every file

---
+++ naturePhotos.js
@@ -0,0 +1,36 @@
+// this is my image array. hoorayy.
+const naturePhotos = [
+    {
+        title: "photo1",
+        description: "This one is very neat",
+        credit: "https://unsplash.com/photos/MF16lGb95WY",
+        image_path: ""
+    },
+    {
+        title: "photo2",
+        description: "Neater",
+        credit: "https://unsplash.com/photos/DuiPYwz3CBA",
+        image_path: "",
+    },
+    {
+        title: "photo3",
+        description: "Neatest",
+        credit: "https://unsplash.com/photos/o_-gToPk62c",
+        image_path: "",
+    },
+    {
+        title: "photo4",
+        description: "Neatester",
+        credit: "https://unsplash.com/photos/zwzeorQPepo",
+        image_path: "",
+    },
+    {
+        title: "photo5",
+        description: "Neatesterest",
+        credit: "https://unsplash.com/photos/EKLr56NaJDw",
+        image_path: "",
+    },
+]
+
+// I wanna export this as a module to use in my server.
+module.exports = stadiums;
--- server.js
+++ server.js
@@ -1,4 +1,67 @@
 const app = require("express");
 const ejs = require("ejs");
+const naturePhotos = require("./naturePhotos");
 require("dotenv").config();
+//requiring all the dependencies, and modules here^
 
+// time to set my view engine!!!
+
+app.set("view engine", "ejs");
+// this here will render my static files with express.
+app.use(express.static(path.join(__dirname, "public")));
+
+// parsing requests
+app.use(express.urlencoded({ extended: true }));
+
+//this is my index page endpoint. 
+// I tend to forget this: req, res means request and response. This is how computers talk to each other, sending requests and responses.
+app.get("/", (req, res) => {
+    res.render("pages/index",
+    {
+        title: "index",
+        current: "page-index",
+        tagline: "this is something, will change later."
+    });
+});
+
+// Now index is done, let's do gallery.
+app.get("/gallery", (req, res) => {
+    res.render("pages/gallery",
+    {
+        title: "Gallery",
+        current: "page-gallery",
+        tagline: "Our favourite pictures of nature"
+    });
+});
+// now, here is our subscribe endpoint.
+app.get("/subscribe", (req, res) => {
+    res.render("pages/subscribe",
+    {
+        title: "subscribe",
+        current: "page-subscribe",
+        tagline: "Subscribe here"
+    });
+}); 
+
+// this will be my JSON endpoint.
+app.get("/api/v0/gallery", (req, res) => {
+    res.json(naturePhotos);
+});
+app.post("/peoples", (req, res) => {
+    res.send(`<p> Thank you, We will send you copies of our nature blogs to your email.</p>`);
+}); 
+
+// If anyone puts in a wrong endpoint, or if anything wrong happens, this code will show an error 404
+app.use(function(req, res) {
+    res.status(404);
+    res.send("404: File not Found");
+});
+
+// I wanna set a PORT variable with the port 3000.
+// definitely learned my mistake with making sure to capitalize PORT... hahahaaha....
+const PORT = process.env.PORT || 3000;
+
+// Listen on PORT and console.log PORT value
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
+});
